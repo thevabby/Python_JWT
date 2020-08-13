9e14af645255dafe1d9ac7d7ff53d2acf8138c54
Vaibhav Dubey <thevabby@gmail.com>
2020-08-13 21:28:34 +1000

    created decorated function

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 from flask import Flask, request, jsonify, make_response
 import jwt
 import datetime
+from functools import wraps
 
 app = Flask(__name__)
 
 app.config["secret_key"] = "thisissecretkey"
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get(
+            "token"
+        )  # http://localhost:5000/route?token=edugdifuhrt.w45fgdrt3.45.fdd.rte4e.ggrf.dfg
+
+        if not token:
+            return jsonify({"message": "Token is missing!"}), 403
+
+        try:
+            data = jwt.decode(token, app.config["secret_key"])
+        except:
+            return jsonify({"message": "Token is invalid"}), 403
+
+        return f(*args, **kwargs)
+
+    return decorated
 
 
 @app.route("/")
